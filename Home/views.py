190440282b090a1dc322c76f44ecbf084d7dc806
Fakhre Alam <fakhrealam9537@gmail.com>
2022-06-27 15:51:55 +0530

    webapp

---
+++ Home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'Home/index.html')
+
+def about(request):
+    return render(request, 'Home/about.html')
+
+
+def contact(request):
+    return render(request, 'Home/contact.html')
